f25739ccd2fbf616201ba21a9b006b4fc6e419a4
codejutsu1 <codejutsu@protonmail.com>
2024-01-08 16:16:00 +0100

    created a HttpResponse trait

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -30,9 +30,9 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        
     }
 
     /**
---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    protected function success($data, $code = 200)
+    {
+        return response()->json(['data' => $data, 'code' => $code], $code);
+    }
+
+    protected function error($error, $code)
+    {
+        return response()->json(["error" => $error, "code" => $code], $code);
+    }
+
+    protected function message($message, $code=200)
+    {
+        return response()->json(["message" => $message, "code" => $code], $code);
+    }
+}
