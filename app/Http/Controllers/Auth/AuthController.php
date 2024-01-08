57a2fc4da0136ca249bc83dd69568310db1f3660
codejutsu1 <codejutsu@protonmail.com>
2024-01-08 21:54:28 +0100

    work on the login and logout functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Services\UserService;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
@@ -22,5 +24,25 @@ class AuthController extends Controller
         $token = $user->createToken('API Token of ' . $user->name)->plainTextToken;
 
         return $this->success([new UserResource($user), 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid Login Details', 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('API Token of ' . $user->name)->plainTextToken;
+
+        return $this->success([new UserResource($user), 'token' => $token], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->success('Successfully logged out.');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function(){ 
     Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 
     Route::apiResource('users', UserController::class);
 });
