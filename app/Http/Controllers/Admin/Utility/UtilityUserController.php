a0a1e93be8f5d0530688165c4844a6dafd53f358
codejutsu1 <codejutsu@protonmail.com>
2024-02-11 20:56:02 +0100

    created all controllers for eacjh mod models

---
+++ app/Http/Controllers/Admin/Utility/UtilityUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Utility;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UtilityUserController extends Controller
+{
+    //
+}
