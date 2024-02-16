9be7498dca6a63e046541f711197aa92d591658d
codejutsu1 <codejutsu@protonmail.com>
2024-02-16 10:57:24 +0100

    wrote test to check for Group package endpoints and Group utility endpoints

--- tests/Feature/Admin/Group/GroupPackageTest.php
+++ tests/Feature/Admin/Group/GroupPackageTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Group;
+use App\Http\Resources\PackageResource;
+
 beforeEach(function() {
     $this->user = createUser();
     $this->admin = createUser(isAdmin: 1);
@@ -20,4 +23,58 @@ test('user can not access the Group Package endpoint', function () {
 test('Unauthenticated user can not access the Group Package endpoint', function () {
     $response = $this->get('/api/v1/admin/groups/1/packages')
                     ->assertStatus(302);
+});
+
+test('Group package endpoint returns data packages', function(){
+    $packages = Group::findOrFail(1)
+                        ->utilities()
+                        ->whereHas('packages')
+                        ->with('packages')
+                        ->get()
+                        ->pluck('packages')
+                        ->collapse();
+    
+    $resource = PackageResource::collection($packages);
+
+    $response = $this->actingAs($this->admin)
+                        ->get('/api/v1/admin/groups/1/packages')
+                        ->assertJson(
+                            $resource->response()->getData(true)
+                        );
+});
+
+test('Group package endpoint returns cable packages', function(){
+    $packages = Group::findOrFail(2)
+                        ->utilities()
+                        ->whereHas('packages')
+                        ->with('packages')
+                        ->get()
+                        ->pluck('packages')
+                        ->collapse();
+    
+    $resource = PackageResource::collection($packages);
+
+    $response = $this->actingAs($this->admin)
+                        ->get('/api/v1/admin/groups/2/packages')
+                        ->assertJson(
+                            $resource->response()->getData(true)
+                        );
+});
+
+test('Group package endpoint returns utilities packages', function(){
+    $packages = Group::findOrFail(3)
+                        ->utilities()
+                        ->whereHas('packages')
+                        ->with('packages')
+                        ->get()
+                        ->pluck('packages')
+                        ->collapse();
+    
+    $resource = PackageResource::collection($packages);
+
+    $response = $this->actingAs($this->admin)
+                        ->get('/api/v1/admin/groups/3/packages')
+                        ->assertJson(
+                            $resource->response()->getData(true)
+                        );
 });
--- tests/Feature/Admin/Group/GroupUtilityTest.php
+++ tests/Feature/Admin/Group/GroupUtilityTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Group;
+use App\Http\Resources\UtilityResource;
+
 beforeEach(function() {
     $this->user = createUser();
     $this->admin = createUser(isAdmin: 1);
@@ -20,4 +23,40 @@ test('user can not access the Group Utility endpoint', function () {
 test('Unauthenticated user can not access the Group Utility endpoint', function () {
     $response = $this->get('/api/v1/admin/groups/1/utilities')
                     ->assertStatus(302);
+});
+
+test('Group utilities endpoint returns data utilites', function() {
+    $utilities = Group::findOrFail(1)->utilities;
+    
+    $resource = UtilityResource::collection($utilities);
+
+    $response = $this->actingAs($this->admin)
+                    ->get('/api/v1/admin/groups/1/utilities')
+                    ->assertJson(
+                        $resource->response()->getData(true)
+                    );
+});
+
+test('Group utilities endpoint returns cable utilites', function() {
+    $utilities = Group::findOrFail(2)->utilities;
+    
+    $resource = UtilityResource::collection($utilities);
+
+    $response = $this->actingAs($this->admin)
+                    ->get('/api/v1/admin/groups/2/utilities')
+                    ->assertJson(
+                        $resource->response()->getData(true)
+                    );
+});
+
+test('Group utilities endpoint returns utilities utilites', function() {
+    $utilities = Group::findOrFail(3)->utilities;
+    
+    $resource = UtilityResource::collection($utilities);
+
+    $response = $this->actingAs($this->admin)
+                    ->get('/api/v1/admin/groups/3/utilities')
+                    ->assertJson(
+                        $resource->response()->getData(true)
+                    );
 });
